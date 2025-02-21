3bcda12df003346bc7496e9dcce716c720986eee
cherop-26 <cherrymutai@gmail.com>
2025-02-21 14:42:02 +0300

    fourth save

--- Quickapp/views.py
+++ Quickapp/views.py
@@ -10,3 +10,7 @@ def about(request):
   return render(request,'about.html')
 def form(request):
   return render(request,'form.html')
+def product(request):
+  return render(request,'product.html')
+def services(request):
+  return render(request,'services.html')
